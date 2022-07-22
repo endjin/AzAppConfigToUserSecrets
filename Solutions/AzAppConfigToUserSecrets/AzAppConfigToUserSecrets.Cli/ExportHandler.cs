2b07ce23908d9bdcbce40a24e49afb228492875a
Mike Larah <mike.larah@endjin.com>
2022-07-22 18:25:02 +0100

    WIP resolve kv secrets

--- Solutions/AzAppConfigToUserSecrets/AzAppConfigToUserSecrets.Cli/ExportHandler.cs
+++ Solutions/AzAppConfigToUserSecrets/AzAppConfigToUserSecrets.Cli/ExportHandler.cs
@@ -3,58 +3,62 @@
 // </copyright>
 
 using System.CommandLine.Invocation;
+using System.Text.Json;
 using AzAppConfigToUserSecrets.Cli.Infrastructure;
 using Azure;
 using Azure.Data.AppConfiguration;
 using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using Spectre.Console;
 
 namespace AzAppConfigToUserSecrets.Cli;
 
 public static class ExportHandler
 {
-  public static Task<int> ExecuteAsync(
-    string tenantId, 
-    string userSecretsId,
-    string endpoint,
-    string connectionString, 
-    bool useCredentials,
-    ICompositeConsole console,
-    InvocationContext context = null)
-  {
-    ConfigurationClient? client;
-
-    var userSecretsStore = new UserSecretsStore(userSecretsId);
-
-    var secrets = userSecretsStore.Load();
-
-    if (useCredentials)
+    public static Task<int> ExecuteAsync(
+      string tenantId,
+      string userSecretsId,
+      string endpoint,
+      ICompositeConsole console,
+      InvocationContext context = null)
     {
-      var credentials = new DefaultAzureCredential(new DefaultAzureCredentialOptions
-      {
-        InteractiveBrowserTenantId = tenantId,
-        SharedTokenCacheTenantId = tenantId,
-        VisualStudioCodeTenantId = tenantId,
-        VisualStudioTenantId = tenantId,
-      });
+        ConfigurationClient? configurationClient;
 
-      client = new ConfigurationClient(new Uri(endpoint), credentials);
+        var userSecretsStore = new UserSecretsStore(userSecretsId);
+
+        var secrets = userSecretsStore.Load();
+
+        var credentials = new DefaultAzureCredential(new DefaultAzureCredentialOptions
+        {
+            InteractiveBrowserTenantId = tenantId,
+            SharedTokenCacheTenantId = tenantId,
+            VisualStudioCodeTenantId = tenantId,
+            VisualStudioTenantId = tenantId,
+        });
+
+        configurationClient = new ConfigurationClient(new Uri(endpoint), credentials);
+
+        var selector = new SettingSelector { Fields = SettingFields.All };
+        Pageable<ConfigurationSetting> settings = configurationClient.GetConfigurationSettings(selector);
+
+        foreach (ConfigurationSetting setting in settings)
+        {
+            if (setting.ContentType == "application/vnd.microsoft.appconfig.keyvaultref+json;charset=utf-8")
+            {
+                Uri secretUri = new Uri(JsonSerializer.Deserialize<JsonElement>(setting.Value).GetProperty("uri").GetString());
+                Uri vaultUri = new Uri(secretUri.GetLeftPart(UriPartial.Authority));
+                string secretName = ""; // TODO: pull out secret name from URI. What if there is an explicit version in there?
+
+                SecretClient secretClient = new SecretClient(vaultUri, credentials);
+                var secret = await secretClient.GetSecretAsync(secretName);
+                // TODO set value
+            }
+
+            console.WriteLine($"{setting.Key} / {setting.Value}");
+            userSecretsStore.Set(setting.Key, setting.Value);
+        }
+
+        userSecretsStore.Save();
+        return Task.FromResult(0);
     }
-    else
-    {
-      client = new ConfigurationClient(connectionString);
-    }
-
-    var selector = new SettingSelector { Fields = SettingFields.All };
-    Pageable<ConfigurationSetting> settings = client.GetConfigurationSettings(selector);
-
-    foreach (ConfigurationSetting setting in settings)
-    {
-      console.WriteLine($"{setting.Key} / {setting.Value}");
-      userSecretsStore.Set(setting.Key, setting.Value);
-    }
-
-    userSecretsStore.Save();
-    return Task.FromResult(0);
-  }
 }
